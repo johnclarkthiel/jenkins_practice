da9a91a0bdfdfcca46fd3268473c94db07a339c8
John <johnclarkthiel@Johns-MacBook-Pro.local>
2018-10-15 22:15:38 -0400

    Adding jenkins file

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,6 @@ require 'yaml'
 require 'selenium-webdriver'
 require 'watir'
 
-puts 'Hello World!'
 tickers = YAML.load_file('tickers.yml')['Tickers']
 puts tickers
 
@@ -13,14 +12,23 @@ closing_prices = []
 
 @browser = Watir::Browser.new :chrome
 @browser.goto 'https://finance.yahoo.com/'
-@search_box = @browser.input(xpath: '//input[@aria-label="Search" and @name="p"]')
-											.wait_until_present(timeout: 60)
-											.flash
-@search_button = @browser.button(id: 'search-button')
-												 .wait_until_present(timeout: 60)
-												 .flash
 
 tickers.each do |ticker|
+	@browser.input(xpath: '//input[@aria-label="Search" and @name="p"]')
+			  .wait_until_present(timeout: 30)
+			  .send_keys ticker
+	@browser.button(id: 'search-button')
+				  .wait_until_present(timeout: 30)
+	        .click
+	price = @browser.span(xpath: '//*[@id="quote-header-info"]/div[3]/div/div/span')
+									 .wait_until_present(timeout: 30)
+									 .text
+	closing_prices.push(price)
+end
+
+closing_prices.each { |e| puts e  }
+
+
 	# https://finance.yahoo.com/quote/BAC?p=BAC&.tsrc=fin-srch
 	# puts "https://finance.yahoo.com/quote/#{ticker}?p=#{ticker}&.tsrc=fin-srch"
 	# @resp = HTTParty.get("https://finance.yahoo.com/quote/#{ticker}?p=#{ticker}&.tsrc=fin-srch")
@@ -31,7 +39,3 @@ tickers.each do |ticker|
 	# puts ticker_price
 	# @driver = Selenium::WebDriver.for :chrome
 	# @driver.navigate.to 'https://finance.yahoo.com/'
-	@search_box.send_keys ticker
-	@search_button.click
-	sleep 5
-end
